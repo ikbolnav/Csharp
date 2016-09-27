2c6bf96f71ea83a605823b50d43c07920da229be
Ikbolnav <ikbolnav@gmail.com>
2016-09-27 10:02:02 +0500

    first commit

--- d1/Program.cs
+++ d1/Program.cs
@@ -11,7 +11,7 @@ namespace d1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello life!");
-            Console.ReadKey();
+            Console.ReadKey();  //this is coment
         }
     }
 }
